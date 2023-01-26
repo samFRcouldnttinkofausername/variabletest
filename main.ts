20887bea2899b4e56ca9f73e4db889d1ba90688b
samFRcouldnttinkofausername <122544219+samFRcouldnttinkofausername@users.noreply.github.com>
2023-01-26 09:38:50 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+    basic.showNumber(counter, 50)
+})
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+    basic.showNumber(counter, 50)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+    basic.showNumber(counter, 50)
+})
+let counter = 0
+counter = 0
+basic.showNumber(counter, 50)
